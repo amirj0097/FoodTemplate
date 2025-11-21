706aa30e8b55e1df719bf6e473d6e618c1da394e
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-11-21 14:31:28 +0330

    Add folder Headers

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ function App() {
   return (
       <div className="w-[1439px] min-h-[5280px]">
 
+        <div className="w-[1440px] h-[120px]">
+          gh
+        </div>
+
+        <div className="w-[1671px] h-[715px]">
+uf
+        </div>
+
       </div>
   )
 }
