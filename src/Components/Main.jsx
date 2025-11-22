7b60a061c3a7703d73a311c55348bb3976a4e302
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-11-22 17:40:02 +0330

    Add main text

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,18 +5,16 @@ export default function Main() {
     return (
         <div className="w-full flex  h-[715px] ">
 
-            <div className="flex flex-col mt-45 w-1/2  items-center  ">
+            <div className="flex flex-col mt-15 w-1/2  items-center   ">
                 <span><img className="mr-140" src="src/icon/Rang.svg" alt="" /></span>
-
-
                 <img src="src/img/Title.svg" alt="" />
-
+                <img className="ml-5 mt-5" src="src/img/Text.svg" alt="" />
 
             </div>
 
 
 
-            <div className="w-1/2 flex justify-end items-end">
+            <div className="w-1/2 flex justify-end items-center">
                 <span><img src="src/img/Sushee.svg" alt="" /></span>
             </div>
 
