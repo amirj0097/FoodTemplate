874061604981645522264f0469f8e2f96acfe034
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-11-22 13:28:49 +0330

    Add icon Rang

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,15 +3,17 @@ export default function Main() {
 
 
     return (
-        <div className="w-full items-center flex h-[715px]">
-
-            <div className="w-1/2">
+        <div className="w-full  flex h-[715px] bg-blue-300">
+            <div className="mt-30 w-1/2">
+                <div className="flex flex-col items-center  ">
+                    <span><img src="src/icon/Rang.svg" alt="" /></span>
                     <p>All You Need For </p>
                     <p>Perfect   Breakfast</p>
+                </div>
+
             </div>
 
-
-            <div className="w-1/2">
+            <div className="w-1/2 flex items-center">
                 <span><img src="src/img/Sushee.svg" alt="" /></span>
             </div>
 
