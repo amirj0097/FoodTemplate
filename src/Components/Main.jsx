18e4509f6a623a262bb56fb635b349b3baeb9e8a
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-11-23 10:39:14 +0330

    Add the icon to the left of the slider.

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -7,10 +7,14 @@ export default function Main() {
 
 
 
-            <div className="flex flex-col mt-15 w-1/2 ml-23 items-start   ">
+            <div className="flex flex-col mt-15 w-1/2 ml-30 items-start   ">
+
+
                 <span className="-ml-3 absolute"><img className="" src="src/icon/Rang.svg" alt="" /></span>
                 <img src="src/img/Title.svg" alt="" />
                 <img className="ml-0 mt-5" src="src/img/Text.svg" alt="" />
+
+                    <span><img className="py-4 px-5 cursor-pointer border border-[#E21A43] absolute left-8 bottom-auto top-90" src="src/icon/left.svg" alt="" /></span>
 
                 <div className="flex w-full h-30 gap-6 mt-5">
                     <a href="" className="bg-[#E21A43] text-white flex items-center max-h-12 px-6 font-semibold">BUY NOW</a>
@@ -22,9 +26,9 @@ export default function Main() {
 
 
 
-            <div className="w-1/2 flex  justify-end items-center">
+            <div className="w-1/2 flex  justify-end items-center mb-10">
                 <span><img src="src/img/Sushee.svg" alt="" /></span>
-                <span><img className="bg-[#E21A43] py-5 top-100 bottom-auto px-6 right-8 absolute" src="src/icon/right.svg" alt="" /></span>
+                <span><img className="bg-[#E21A43] cursor-pointer py-4 top-90 bottom-auto px-5 right-8 absolute" src="src/icon/right.svg" alt="" /></span>
             </div>
 
 
