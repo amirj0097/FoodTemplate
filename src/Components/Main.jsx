789f67959859d1f5c517e61e2ced9bbaca4f67bd
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-11-23 10:00:47 +0330

    Add icon gool

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,14 +5,15 @@ export default function Main() {
     return (
         <div className="w-full flex  h-[715px] ">
 
-            <div className="flex flex-col mt-15 w-1/2  items-center   ">
-                <span><img className="mr-140" src="src/icon/Rang.svg" alt="" /></span>
+            <div className="flex flex-col mt-15 w-1/2 ml-23 items-start   ">
+                <span className="-ml-3 absolute"><img className="" src="src/icon/Rang.svg" alt="" /></span>
                 <img src="src/img/Title.svg" alt="" />
-                <img className="ml-5 mt-5" src="src/img/Text.svg" alt="" />
+                <img className="ml-0 mt-5" src="src/img/Text.svg" alt="" />
 
-                <div className="flex mr-auto ml-21 gap-6 mt-5">
-                    <a href="" className="bg-[#E21A43] text-white py-2 px-4">BUY NOW</a>
-                    <a href="" className="text-[#E21A43] flex items-center py-2 px-4 border border-[#E21A43]">SEE MORE</a>
+                <div className="flex w-full h-30 gap-6 mt-5">
+                    <a href="" className="bg-[#E21A43] text-white flex items-center max-h-12 px-6 font-semibold">BUY NOW</a>
+                    <a href="" className="text-[#E21A43] flex items-center max-h-12 px-6 font-semibold border border-[#E21A43]">SEE MORE</a>
+                    <img className="ml-auto -mt-5 mr-auto w-10" src="src/icon/gool.svg" alt="" />
                 </div>
 
             </div>
