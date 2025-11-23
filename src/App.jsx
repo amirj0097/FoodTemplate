28a253368c84caae416970e5893796e21e05be17
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-11-23 12:05:53 +0330

    Component main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Main from "./Components/Main";
+import Categories from "./Components/Categories";
+import Main from "./Components/Main1/Main";
 import Header from "./Headers/Header";
 
 function App() {
@@ -13,16 +14,7 @@ function App() {
 
       <Main />
 
-      <div className="flex flex-row-reverse absolute bg-white shadow-2xl shadow-gray-400 items-center justify-center gap-19 py-8 -mt-12.5 ml-50  w-[70%]  px-8">
-        <a href=""><img src="src/img/Sweets.svg" alt="Sweets" /></a>
-        <a href=""><img src="src/img/Pasta.svg" alt="Pasta" /></a>
-        <a href=""><img src="src/img/Meat.svg" alt="Meat" /></a>
-        <a href=""><img src="src/img/Bakery.svg" alt="Bakery" /></a>
-        <a href=""><img src="src/img/Semi-finished.svg" alt="Semi-finished" /></a>
-        <a href=""><img src="src/img/Vegetables.svg" alt="Vegetables" /></a>
-        <a href=""><img src="src/img/Fruits.svg" alt="Fruits" /></a>
-
-      </div>
+      <Categories />
 
       <div className="bg-[#F7F7F7] w-full h-900">
         pijpo
