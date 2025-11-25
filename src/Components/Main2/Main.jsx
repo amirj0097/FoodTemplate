a93cba94132598de54597524bc0f25a0e4f3f666
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-11-25 16:59:29 +0330

    Add text poster-1

--- src/Components/Main2/Main.jsx
+++ src/Components/Main2/Main.jsx
@@ -20,15 +20,18 @@ export default function Main() {
           </div>
 
 
-          <div className="bg-white w-60 h-100 mt-auto mb-auto flex justify-center  items-start">
+          <div className="bg-white w-60 h-90 mt-auto mb-auto flex justify-center  items-start">
             <img className="p-3" src="src/img/Noone.svg" alt="" />
-            <div className="flex w-30 px-2 py-2 gap-2 absolute justify-center mt-[235px] bg-white shadow-md shadow-gray-200">
+            <div className="flex w-30 px-2 py-2 gap-2 absolute justify-center mt-[233px] bg-white shadow-md shadow-gray-200">
 
              <a className=" " href=""> <img className="border-r-1 border-[#888888]  w-15 pr-2" src="src/img/+.svg" alt="" /></a>
              <a className=" " href=""> <img className="border-r-1 border-[#888888] w-15 pr-2" src="src/icon/charkhesh.svg" alt="" /></a>
              <a className=" " href=""> <img className="border-r-1 border-[#888888] w-15   pr-2" src="src/icon/Like.svg" alt="" /></a>
              <a className="" href=""> <img className="w-10 pr-0" src="src/icon/search (2).svg" alt="" /></a>
 
+            </div>
+            <div className="absolute mt-70 w-50">
+              <img src="src/img/textnoone.svg" alt="" />
             </div>
           </div>
 
