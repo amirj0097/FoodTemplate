240f68ee0c1a31a7124ae61cd63c17a66c960592
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-11-25 13:37:21 +0330

    Add categories and new products section

--- src/Components/Main2/Main.jsx
+++ src/Components/Main2/Main.jsx
@@ -1,7 +1,32 @@
-export default function Main(){
-    return(
-        <div className="bg-[#F7F7F7] w-full flex items-center justify-center h-[500px]">
-          <img className="ml-50" src="src/img/name main.svg" alt="" />
+export default function Main() {
+  return (
+    <div className="bg-[#a4dfdb] w-full items-center justify-center flex h-[900px]">
+      <div className="flex flex-col gap-y-20">
+        <img className="" src="src/img/name main.svg" alt="New products" />
+
+
+        <div className="flex  bg-amber-300">
+
+          <div className="bg-white flex items-center flex-col pb-7  p-3 w-70">
+            <div className="flex">
+              <img className="z-6" src="src/img/sarfasl.svg" alt="" />
+              <img className="-ml-13 h-93 mt-5" src="src/img/text-sarfasl.svg" alt="" />
+            </div>
+            <img src="src/img/Title-sarfasl.svg" alt="" />
+            <div className="bg-[#E21A431A] mt-3 px-6.5 py-4">
+             <a href=""> <img src="src/img/Order Now.svg" alt="" /></a>
+            </div>
+          </div>
+
+
+
+
+          {/* <div className=""></div>
+            <div className=""></div>
+            <div className=""></div> */}
+
+        </div>
       </div>
-    )
+    </div>
+  )
 }
