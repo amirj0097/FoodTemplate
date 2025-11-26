b6491386dac1da0ed05843359ec38a31a39b61da
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-11-26 16:54:55 +0330

    Add the price the poster1

--- src/Components/Main2/Main.jsx
+++ src/Components/Main2/Main.jsx
@@ -33,6 +33,13 @@ export default function Main() {
             <div className="absolute mt-70 w-50">
               <img src="src/img/textnoone.svg" alt="" />
             </div>
+
+              <div className="flex w-50 absolute mt-76 gap-3">
+                <span className="line-through font-semibold">$40.00</span>
+                <span className="font-bold">$30.00</span>
+                <span>(1kg)</span>
+              </div>
+
           </div>
 
         </div>
