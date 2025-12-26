5f305fd39df2ca8f9c94f3b99546a5bf916dadbc
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-12-26 17:59:56 +0330

    Add Text Main7

--- src/Components/Main7/Main.jsx
+++ src/Components/Main7/Main.jsx
@@ -1,6 +1,6 @@
 
 import Title from '/src/img/Title Main7.svg'
-
+import Text from '/src/img/Text Main7.svg'
 
 
 export default function Main() {
@@ -9,9 +9,13 @@ export default function Main() {
 
         <div className="h-[650px] flex w-full bg-gray-300 items-center">
 
-                <div className="w-1/2 flex justify-center bg-red-300">
+                <div className="w-1/2 flex flex-col items-center justify-center bg-red-300 gap-y-3">
+
                 <img src={Title} alt="" />
+                <img src={Text} alt="" />
                 </div>
+
+
                 <div className="w-1/2 bg-amber-400">ytfyt</div>
 
         </div>
