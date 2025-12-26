fc883101274cf04f395c8481cdaecf302e8c6f9b
Amir hossein JahanPour <torkashvandamer@gmail.com>
2025-12-26 20:04:29 +0330

    Add photos to the right of the main7 section

--- src/Components/Main7/Main.jsx
+++ src/Components/Main7/Main.jsx
@@ -3,6 +3,10 @@ import Title from '/src/img/Title Main7.svg'
 import Text from '/src/img/Text Main7.svg'
 import Google from '/src/img/googleplay.svg'
 import Appstore from '/src/img/appstore.svg'
+import Appel_left from '/src/img/image left.svg'
+import Appel_Middle from '/src/img/image Middle.svg'
+import Appel_right from '/src/img/image right.svg'
+
 
 export default function Main() {
 
@@ -25,7 +29,13 @@ export default function Main() {
             </div>
 
 
-            <div className="w-1/2 bg-amber-400">ytfyt</div>
+            <div className="w-1/2 flex justify-center items-center">
+
+                <img className='absolute left-auto right-92' src={Appel_left} alt="" />
+                <img className='relative z-10 flex justify-center items-center' src={Appel_Middle} alt="" />
+                <img className='absolute right-19' src={Appel_right} alt="" />
+
+            </div>
 
         </div>
 
